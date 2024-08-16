533e59deb2585bc2fac2774eeabc9fb3c2df41c3
Huy Nguyen <huynguyen560@gmail.com>
2024-08-15 18:09:52 -0700

    feat: add listener to button to draw new grid - remove old grid and draw new grid with user's input size - add logic to check if user's input is a valid size
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
-const CONTAINER_SIZE = 650;
+const CONTAINER_SIZE = 600;
 
 const containerDiv = document.querySelector(".container");
 
-const gridLength = 16;
+const newGridButton = document.querySelector(".new-grid-btn");
 
-const heightAndWidthSize = CONTAINER_SIZE / gridLength;
+newGridButton.addEventListener("click", () => {
+  // Remove previous grid
+  containerDiv.replaceChildren();
 
-// Draw the grid
-for (let i = 0; i < gridLength; i++) {
-  const newRow = document.createElement("div");
-  newRow.classList.add("row");
-  for (let j = 0; j < gridLength; j++) {
-    const newSquare = document.createElement("div");
-    newSquare.style.width = `${heightAndWidthSize}px`;
-    newSquare.style.height = `${heightAndWidthSize}px`;
-    newSquare.style.border = "1px solid black";
-    newRow.appendChild(newSquare);
+  const gridLength = Number.parseInt(
+    prompt("Enter grid size. Maximum size is 100")
+  );
+
+  if (gridLength > 100 || gridLength < 1) {
+    alert("Invalid grid size. Must be between 1 and 100");
+  } else {
+    const heightAndWidthSize = CONTAINER_SIZE / gridLength;
+
+    // Draw the grid
+    for (let i = 0; i < gridLength; i++) {
+      const newRow = document.createElement("div");
+      newRow.classList.add("row");
+      for (let j = 0; j < gridLength; j++) {
+        const newSquare = document.createElement("div");
+        newSquare.style.width = `${heightAndWidthSize}px`;
+        newSquare.style.height = `${heightAndWidthSize}px`;
+        newSquare.style.border = "1px solid black";
+        newRow.appendChild(newSquare);
+      }
+
+      containerDiv.appendChild(newRow);
+
+      const squares = document.querySelectorAll(".row div");
+
+      squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+          square.classList.add("highlight");
+        });
+      });
+    }
   }
-
-  containerDiv.appendChild(newRow);
-}
-
-const squares = document.querySelectorAll(".row div");
-
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.classList.add("highlight");
-  });
 });
